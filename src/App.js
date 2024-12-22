4bbc346f0b52977d6b5ccb19c51ab0c05a64578b
Jaheim <jc41809@georgiaSouthern.edu>
2024-12-22 02:26:59 -0500

    First commit
    Revamping of the first algorithm visualizer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import react from 'react';
+import React, {Component} from 'react'
+
+import Bar from './components/Bar';
+
+
+//CSS imports
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+  state = {  
+    array: [],
+    arraySteps: [],
+    colorKey: [],
+    colorSteps: [],
+    currentStep: 0,
+    count: 10,
+    delay: 100,
+    algorithm: '',
+    timeouts: [],
+
+  } 
+
+  componentDidMount(){
+    this.generateRandomArray();
+
+  }
+  GenerateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+
+  }
+
+  generateRandomArray = () => {
+    const count = this.state.count;
+    const temp = [];
+    for (let i = 0; i < count; i++){
+      temp.push(this.GenerateRandomNumber(50, 200));
+
+    }
+
+    this.setState({
+      array: temp,
+      arraySteps: [temp],
+      currentStep: 0
+    });
+
+
+  };
+
+  changeArray =  (index,value) => {
+    let arr = this.state.array;
+    arr[index] = value;
+    this.setState({
+      array: arr,
+      arraySteps: [arr],
+      currentStep: 0
+
+    })
+
+  }
+
+
+  render() { 
+
+    let bars = this.state.array.map((value, index) => ( 
+      <Bar 
+        key = {index}
+        index = {index}
+        length = {value}
+        color = {0}
+        changeArray = {this.changeArray}
+        />
+      ));
+
+    return (
+    <div className='app'>
+        <div className="frame">
+          <div className="barsDiv container card">
+            {bars}
+          </div>
+        </div> 
+        <div className ="control-panel"></div>
+        <div className = "panel"> </div>
     </div>
-  );
+    );
+  }
 }
+ 
+
 
 export default App;
